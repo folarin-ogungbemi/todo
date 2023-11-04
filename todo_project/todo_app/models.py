d486dbdbd3732de236f8b72cc3c40ec25b705904
folarin-ogungbemi <ftogungbemi@gmail.com>
2023-11-04 16:42:42 +0100

    Add date and content field to model and migrate

--- todo_project/todo_app/models.py
+++ todo_project/todo_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 
@@ -6,7 +7,9 @@ from django.db import models
 class Todo(models.Model):
 
     name = models.CharField(max_length=50, null=True, blank=True)
+    content = models.CharField(max_length=225, null=True, blank=True)
     done = models.BooleanField(null=True, blank=True, default=False)
+    created_on = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.name
