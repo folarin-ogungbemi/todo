16f7495fdb40cd9054925e06f2836b0b994a0034
folarin-ogungbemi <ftogungbemi@gmail.com>
2023-11-04 15:57:05 +0100

    Create model for todo and migrate

--- todo_project/todo_app/models.py
+++ todo_project/todo_app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Todo(models.Model):
+
+    name = models.CharField(max_length=50, null=True, blank=True)
+    done = models.BooleanField(null=True, blank=True, default=False)
+
+    def __str__(self):
+        return self.name
