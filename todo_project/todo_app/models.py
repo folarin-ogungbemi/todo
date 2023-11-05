ceeb036ddbd1375ad2f04a01ea463b3e355c6eb8
folarin-ogungbemi <ftogungbemi@gmail.com>
2023-11-05 13:35:59 +0100

    change field to required

--- todo_project/todo_app/models.py
+++ todo_project/todo_app/models.py
@@ -6,9 +6,9 @@ from django.utils import timezone
 
 class Todo(models.Model):
 
-    name = models.CharField(max_length=50, null=True, blank=True)
-    content = models.CharField(max_length=225, null=True, blank=True)
-    done = models.BooleanField(null=True, blank=True, default=False)
+    name = models.CharField(max_length=50)
+    content = models.CharField(max_length=225, null=False, blank=False)
+    done = models.BooleanField(default=False)
     created_on = models.DateTimeField(auto_now=True)
 
     def __str__(self):
